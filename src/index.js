24c49e498163e4b2294a6a405b7e328b2c95a058
lstar <lodestar.sr@gmail.com>
2022-03-24 23:23:28 +0100

    convert functional component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import config from "./config";
 import Home from './components/home';
 
 render(
-      <BrowserRouter>
-        <Switch>
-          <Route render={(routeProps) =>
-              <Home {...routeProps} {...config}/>}/>
-        </Switch>
-      </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route render={(routeProps) =>
+        <Home {...routeProps} {...config} />} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
